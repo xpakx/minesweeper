ef034805c6a5b7e4f82245f59318000911acb922
xpakx <55586470+xpakx@users.noreply.github.com>
2022-02-20 23:15:28 +0100

    feat(frontend): load player's games

--- frontend/src/app/component/player/player.component.ts
+++ frontend/src/app/component/player/player.component.ts
@@ -1,4 +1,8 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { GameInfo } from 'src/app/entity/game-info';
+import { GameService } from 'src/app/service/game.service';
 
 @Component({
   selector: 'app-player',
@@ -6,10 +10,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./player.component.css']
 })
 export class PlayerComponent implements OnInit {
+  public invalid: boolean = false;
+  public message: string = '';
 
-  constructor() { }
+  public games: GameInfo[] = [];
+
+  constructor(private service: GameService, private router: Router, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.route.params.subscribe(routeParams => {
+      this.loadPlayer(routeParams.id);
+    });  
+  }
+
+  loadPlayer(id: number): void {
+    this.service.getGamesByPlayerId(id).subscribe(
+      (response: GameInfo[]) => {
+        this.games = response;
+    },
+    (error: HttpErrorResponse) => {
+      if(error.status === 401) {
+        localStorage.removeItem("token");
+      }
+      this.message = error.error.message;
+      this.invalid = true;
+    });
   }
 
 }
--- frontend/src/app/service/game.service.ts
+++ frontend/src/app/service/game.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { Game } from '../entity/game';
+import { GameInfo } from '../entity/game-info';
 import { GameRequest } from '../entity/game-request';
 import { MoveRequest } from '../entity/move-request';
 import { Position } from '../entity/position';
@@ -19,8 +20,8 @@ export class GameService {
     return this.http.get<Game>(`${this.apiServerUrl}/games/${id}`);
   }
 
-  public getGamesByPlayerId(id: number):  Observable<Game[]> {
-    return this.http.get<Game[]>(`${this.apiServerUrl}/players/${id}/games`);
+  public getGamesByPlayerId(id: number):  Observable<GameInfo[]> {
+    return this.http.get<GameInfo[]>(`${this.apiServerUrl}/players/${id}/games`);
   }
 
   public newGame(id: number, request: GameRequest):  Observable<Game> {
