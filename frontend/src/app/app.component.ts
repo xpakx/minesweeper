a3298e6eca2c1b98eedf0fb8599961fa7a56b3e9
xpakx <55586470+xpakx@users.noreply.github.com>
2022-02-21 23:19:29 +0100

    feat(frontend): add option to create new game

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,9 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { Game } from './entity/game';
+import { GameRequest } from './entity/game-request';
+import { GameService } from './service/game.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +12,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'minesweeper';
+  
+  constructor(private service: GameService, private router: Router) { }
+
+  newGame(width: number, height: number) {
+    let request: GameRequest = {width: width, height: height};
+    let playerId: number = Number(localStorage.getItem("user_id"));
+    this.service.newGame(playerId, request).subscribe(
+      (response: Game) => {
+        this.router.navigate(['/games/'+response.id]);
+    },
+    (error: HttpErrorResponse) => {
+      if(error.status === 401) {
+        localStorage.removeItem("token");
+      }
+      //TODO
+    });
+  }
 }
